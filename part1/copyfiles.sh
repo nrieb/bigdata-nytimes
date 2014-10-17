7036222a648d291c420f5d29f17c73677c3ea750
nrieb <nrieb@cs.unm.edu>
2014-10-16 19:31:57 -0600

    copy from local, not put?

--- part1/copyfiles.sh
+++ part1/copyfiles.sh
@@ -4,4 +4,4 @@ HADOOP_HOME="/usr/local/hadoop"
 DATA_PATH="~/bigdata-nytimes/part1"
 DFS_HOME="/users/nrieb/nytimes"
 
-$HADOOP_HOME/bin/hadoop dfs -put $DATA_PATH/articles.json $DFS_HOME/orig/articles.json
+$HADOOP_HOME/bin/hadoop dfs -copyFromLocal $DATA_PATH/articles.json $DFS_HOME/orig/articles.json
